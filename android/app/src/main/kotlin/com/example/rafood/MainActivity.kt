99981106c0c7d18eb8a5835ac4848a8ca73ebe99
NicoDomur <nikkyforever8@gmail.com>
2022-12-01 17:59:39 -0600

    Aplicacion de proyecto final Rafood

---
+++ android/app/src/main/kotlin/com/example/rafood/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.rafood
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/pantalla_principal.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class PantallaPrincipal extends StatefulWidget {
+  const PantallaPrincipal({super.key});
+
+  @override
+  State<PantallaPrincipal> createState() => _PantallaPrincipalState();
+}
+
+class _PantallaPrincipalState extends State<PantallaPrincipal> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Rafood"),
+        actions: [
+          Row(
+            children: [
+              FloatingActionButton.small(
+                heroTag: 'busqueda',
+                disabledElevation: 0,
+                elevation: 0,
+                onPressed: () {},
+                child: const Icon(Icons.search),
+              ),
+              FloatingActionButton.small(
+                heroTag: 'configuracion',
+                onPressed: () {},
+                child: const Icon(Icons.settings),
+              ),
+            ],
+          )
+        ],
+      ),
+      body: const Center(child: Text('Hola')),
+    );
+  }
+}
