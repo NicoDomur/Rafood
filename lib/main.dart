1d8cc62ee7b0527b88aad0e1861c846e2587f7fb
CRGdev11 <crizhan10@gmail.com>
2022-12-10 11:30:33 -0600

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:rafood/firebase_options.dart';
 import 'package:rafood/paginas/food/popular_comida_detalle.dart';
 import 'package:rafood/paginas/food/recomendada_comida_detalle.dart';
 import 'package:rafood/paginas/screens/pantalla_principal.dart';
 import 'package:rafood/theme/temas.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
